40199fb82c340e76f54be114270a058341f4bf72
mazecross <guge4game@gmail.com>
2016-12-04 19:34:12 +0000

    Update 412 Fizz Buzz.java

--- Java/412 Fizz Buzz.java
+++ Java/412 Fizz Buzz.java
@@ -1,4 +1,4 @@
-/#
+/*
 Write a program that outputs the string representation of numbers from 1 to n.
 
 But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
@@ -25,7 +25,7 @@ Return:
     "14",
     "FizzBuzz"
 ]
-#/
+*/
 //xxy 2016.12.4 Manchester
 public class Solution {
     public List<String> fizzBuzz(int n) {
